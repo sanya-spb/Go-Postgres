f9096b089baa85a3d8ea9402b51a5cc80b7e0a2f
Alexander Kupriyanov <sanya-spb@mail.ru>
2021-10-05 00:18:51 +0300

    merge constraint

--- task03/init/01_schema.sql
+++ task03/init/01_schema.sql
@@ -5,13 +5,16 @@ SET ROLE sanya;
 	1. Добавить ограничения foreign keys для всех имеющихся связей между таблицами в БД, созданной в первом занятии
 */
 
+
 -- справочник предоставляемых услуг
 CREATE TABLE services (
 	id serial not null,
 	service text not null,
 	price numeric not null,
 	descr text not null,
-	CONSTRAINT services_pkey PRIMARY KEY (id)
+	CONSTRAINT services_pkey PRIMARY KEY (id),
+	CONSTRAINT services_ukey UNIQUE (service),
+	CONSTRAINT services_check CHECK (price>=0)
 );
 COMMENT ON TABLE public.services IS 'справочник предоставляемых услуг';
 COMMENT ON COLUMN public.services.service IS 'услуга';
@@ -26,7 +29,8 @@ CREATE TABLE personal (
 	lname text not null,
 	phone text not null,
 	email text not null,
-	CONSTRAINT personal_pkey PRIMARY KEY (id)
+	CONSTRAINT personal_pkey PRIMARY KEY (id),
+	CONSTRAINT personal_ukey UNIQUE (fname,lname)
 );
 COMMENT ON TABLE public.personal IS 'персонал';
 COMMENT ON COLUMN public.personal.fname IS 'имя';
@@ -43,7 +47,8 @@ CREATE TABLE personal_service (
 	price numeric not null,
 	CONSTRAINT personal_service_pkey PRIMARY KEY (id),
 	CONSTRAINT personal_service_personal_fkey FOREIGN KEY (personal_id) REFERENCES public.personal(id),
-	CONSTRAINT personal_service_service_fkey FOREIGN KEY (service_id) REFERENCES public.services(id)
+	CONSTRAINT personal_service_service_fkey FOREIGN KEY (service_id) REFERENCES public.services(id),
+	CONSTRAINT personal_service_check CHECK (price>=0)
 );
 COMMENT ON TABLE public.personal_service IS 'специализация персонала (8-8)';
 COMMENT ON COLUMN public.personal_service.price IS 'цена работы (руб/час)';
@@ -56,7 +61,8 @@ CREATE TABLE saunas (
 	phone text not null,
 	contact text not null,
 	email text not null,
-	CONSTRAINT saunas_pkey PRIMARY KEY (id)
+	CONSTRAINT saunas_pkey PRIMARY KEY (id),
+	CONSTRAINT saunas_ukey UNIQUE ("name")
 );
 COMMENT ON TABLE public.saunas IS 'список объектов';
 COMMENT ON COLUMN public.saunas."name" IS 'название';
@@ -75,7 +81,8 @@ CREATE TABLE sauna_service (
 	price numeric not null,
 	CONSTRAINT sauna_service_pkey PRIMARY KEY (id),
 	CONSTRAINT sauna_service_sauna_fkey FOREIGN KEY (sauna_id) REFERENCES public.saunas(id),
-	CONSTRAINT sauna_service_service_fkey FOREIGN KEY (service_id) REFERENCES public.services(id)
+	CONSTRAINT sauna_service_service_fkey FOREIGN KEY (service_id) REFERENCES public.services(id),
+	CONSTRAINT sauna_service_check CHECK (capacity>0 and price>=0)
 );
 COMMENT ON TABLE public.sauna_service IS 'возможные услуги на объекте';
 COMMENT ON COLUMN public.sauna_service.capacity IS 'вместимость (кол-во клиентов)';
@@ -88,7 +95,8 @@ CREATE TABLE orders (
 	peoples_count int not null,
 	duration_common int not null,
 	summary numeric not null,
-	CONSTRAINT orders_pkey PRIMARY KEY (id)
+	CONSTRAINT orders_pkey PRIMARY KEY (id),
+	CONSTRAINT orders_check CHECK (date_start>now() and peoples_count>0 and duration_common>0 and summary>=0)
 );
 COMMENT ON TABLE public.orders IS 'список заказов';
 COMMENT ON COLUMN public.orders.date_start IS 'дата-время начала';
@@ -105,7 +113,8 @@ CREATE TABLE order_services (
 	duration int not null,
 	CONSTRAINT order_services_pkey PRIMARY KEY (id),
 	CONSTRAINT sauna_service_orders_fkey FOREIGN KEY (orders_id) REFERENCES public.orders(id),
-	CONSTRAINT sauna_service_service_fkey FOREIGN KEY (service_id) REFERENCES public.services(id)
+	CONSTRAINT sauna_service_service_fkey FOREIGN KEY (service_id) REFERENCES public.services(id),
+	CONSTRAINT order_services_check CHECK (peoples>0 and duration>0)
 );
 COMMENT ON TABLE public.order_services IS 'услуги по заказу';
 COMMENT ON COLUMN public.order_services.peoples IS 'кол-во человек (клиентов)';
